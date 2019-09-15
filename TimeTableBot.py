708a139adbd91b0ac90aeb00a51f1185a8668e69
joi <khrissianjoi@gmail.com>
2019-09-15 16:44:00 +0100

    styling fix

--- TimeTableBot.py
+++ TimeTableBot.py
@@ -1,10 +1,10 @@
-import requests
 import json
-import datetime
-import dateutil.parser
-import env_secrets
 import os
+import requests
+
+import env_secrets
 from timetable_headers import current, headers
+
 
 link = os.environ.get("TimetableUrl")
 response = requests.post(link, json=current, headers=headers)
@@ -12,5 +12,4 @@ response = requests.post(link, json=current, headers=headers)
 modules = json.loads(response.text)[0]['CategoryEvents']
 
 for module in modules[::-1]:
-  print(module['ExtraProperties'][0]['Value'])
-
+    print(module['ExtraProperties'][0]['Value'])

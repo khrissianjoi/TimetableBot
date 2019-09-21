a7759a98d57175b51a5ec532b96d6fdf614834b1
joi <khrissianjoi@gmail.com>
2019-09-21 23:34:37 +0100

    refactor, different functions

--- TimeTableBot.py
+++ TimeTableBot.py
@@ -7,21 +7,32 @@ import env_secrets
 from TimetableHeaders import current, headers
 
 
-link = os.environ.get("TimetableUrl")
-response = requests.post(link, json=current, headers=headers)
+def timetableRequest():
+    link = os.environ.get("TimetableUrl")
+    response = requests.post(link, json=current, headers=headers)
+    return response
 
-modules = json.loads(response.text)[0]['CategoryEvents']
-time = datetime.datetime.today()
-times= {}
-dates = []
-for module in modules:
-    venue = module['Location']
-    time = datetime.datetime.strptime(module['StartDateTime'], '%Y-%m-%dT%H:%M:%S%z')
-    dates.append(time)
-    times[time] = module['ExtraProperties'][0]['Value'], venue
 
-CurrentTime = datetime.datetime.strptime("2019-09-24T09:00:00+00:00", '%Y-%m-%dT%H:%M:%S%z')
-differences = [abs(CurrentTime - each_date) for each_date in dates]
-minimum = min(differences)
-closest_date = dates[differences.index(minimum)]
-print(times[closest_date])
+def modulesCollector():
+    response = timetableRequest()
+    modules = json.loads(response.text)[0]['CategoryEvents']
+    time = datetime.datetime.today()
+    times = {}
+    dates = []
+    for module in modules:
+        venue = module['Location']
+        time = datetime.datetime.strptime(module['StartDateTime'], '%Y-%m-%dT%H:%M:%S%z')
+        dates.append(time)
+        times[time] = module['ExtraProperties'][0]['Value'], venue
+
+    return closestModuleIdentifier(times, dates)
+
+
+def closestModuleIdentifier(modulesInformation, startDateTimes):
+    CurrentTime = datetime.datetime.strptime("2019-09-24T09:00:00+00:00", '%Y-%m-%dT%H:%M:%S%z')
+    differences = [abs(CurrentTime - each_date) for each_date in startDateTimes]
+    closestDate = startDateTimes[differences.index(min(differences))]
+    
+    return modulesInformation[closestDate]
+
+print(modulesCollector())

3cf783d81a2c3043cc3f488cbe3d01ffc0ffd1eb
joi <khrissianjoi@gmail.com>
2019-09-21 23:41:25 +0100

    refactor, new variable names

--- TimeTableBot.py
+++ TimeTableBot.py
@@ -14,25 +14,25 @@ def timetableRequest():
 
 
 def modulesCollector():
+    '''collects all the lectures in that week'''
     response = timetableRequest()
-    modules = json.loads(response.text)[0]['CategoryEvents']
-    time = datetime.datetime.today()
-    times = {}
-    dates = []
-    for module in modules:
-        venue = module['Location']
-        time = datetime.datetime.strptime(module['StartDateTime'], '%Y-%m-%dT%H:%M:%S%z')
-        dates.append(time)
-        times[time] = module['ExtraProperties'][0]['Value'], venue
+    lecturesOfTheWeek = json.loads(response.text)[0]['CategoryEvents']
+    allLectureStartTime = []
+    lecturesDetails = {}
+    for lecture in lecturesOfTheWeek:
+        venue = lecture['Location']
+        lectureDateTime = datetime.datetime.strptime(lecture['StartDateTime'], '%Y-%m-%dT%H:%M:%S%z')
+        allLectureStartTime.append(lectureDateTime)
+        lecturesDetails[lectureDateTime] = lecture['ExtraProperties'][0]['Value'], venue
 
-    return closestModuleIdentifier(times, dates)
+    return closestModuleIdentifier(lecturesDetails, allLectureStartTime)
 
 
 def closestModuleIdentifier(modulesInformation, startDateTimes):
     CurrentTime = datetime.datetime.strptime("2019-09-24T09:00:00+00:00", '%Y-%m-%dT%H:%M:%S%z')
     differences = [abs(CurrentTime - each_date) for each_date in startDateTimes]
     closestDate = startDateTimes[differences.index(min(differences))]
-    
+
     return modulesInformation[closestDate]
 
 print(modulesCollector())
